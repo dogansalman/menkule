53bdf9ee2fc5e16ef413cdb8242313c354220455
Burak Doğan <me@burak-dogan.com>
2017-07-07 17:36:28 +0300

    Updates

--- src/app/page/main/index.js
+++ src/app/page/main/index.js
@@ -5,4 +5,56 @@ import Footer from '../footer';
 /**
  * Main page
  */
-export default () => Header(false).then(() => Footer()).then(() => $("body").zone("content").setContentAsync(template()));
+export default () => Header(false)
+  .then(() => Footer())
+  .then(() => $("body").zone("content").setContentAsync(template()))
+  .then(template => new Promise(resolve => {
+    /*
+    template.find('.searchcity')
+      .geocomplete({
+        country: ['tr'],
+        types: ['(cities)']
+      })
+      .bind("geocode:result", function(event, result){
+        Map.getLocationViewport(result.name).then((locationDetail) => {
+          location = Object.assign(locationDetail,{'name':result.name});
+        })
+      });
+    template.find('.searchcity').defaultText();
+*/
+    //search
+    template.find('button.seachengine_btn').on('click', (e) => {
+      e.preventDefault();
+      App.isMobile()
+        .then((mobile) => App.promise(() => mobile ? template.find(".search-form-mobile") : template.find(".search-form") )   )
+        .then((searchForm) => $(searchForm).validateFormAsync(searchRules))
+        .then((formData) =>  {
+          App.generateAdvertSearchUrl({
+            'checkin': formData.date.split(' - ')[0].trim(),
+            'checkout': formData.date.split(' - ')[1].trim(),
+            'lat' : location.lat,
+            'lng' : location.lng,
+            'guest' : formData.guest,
+            'name': location.name.turkishToLower()
+          })
+            .then((searchUrl) => App.navigate(searchUrl.url, searchUrl.query));
+        });
+    });
+/*
+    //slide
+    template.find('.menkule_faq').bxSlider({controls: false});
+
+    //create calendar
+    template.find('.calendar').flatpickr(
+      {
+        mode: 'range',
+        minDate: 'today',
+        maxDate: moment(new Date()).add(1, 'year').format('YYYY-MM-DD')
+      }
+    );
+    //init player
+    //player.onYouTubePlayerAPIReady();
+*/
+    $('body').addClass('home');
+    resolve();
+  }));
