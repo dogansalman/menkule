996f6601d32ee5db18a30a55540a5064480f1e45
dogansalman <dogan.salman@yahoo.com>
2017-10-23 17:51:10 +0300

    socket adress

--- src/lib/configs/config.js
+++ src/lib/configs/config.js
@@ -2,7 +2,7 @@ export default () =>{
     return {
         "apiAdress": "http://localhost:9090",
         "cloudinaryBaseUrl": "https://res.cloudinary.com/www-menkule-com-tr/image/upload/",
-        "socketAddress": "http://localhost:9898",
+        "socketAddress": "https://ws.menkule.com.tr",
         "nullImageUrl": "/assets/no-photo.png",
         'secretKey': 'qQrsBccsx0KHjPOuXOYg5sMeJ1GT0uFiwDVvVBrs'
     }
