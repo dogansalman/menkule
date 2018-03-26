cc8295e63a90439fe5aea8c522a184cfa603cc2c
dogansalman <dogan.salman@yahoo.com>
2018-03-26 17:09:19 +0300

    #12 cakisan rez & tasarim iyilestirme

--- src/app/page/user/rezervation/index.js
+++ src/app/page/user/rezervation/index.js
@@ -6,7 +6,7 @@ import Fancybox from  '@fancyapps/fancybox';
 import Message from '../../../modal/message';
 import Confirm from '../../../modal/confirm';
 import AppMessages  from '../../../../lib/appMessages';
-import _Rezervation from '../rezervation';
+import ConfirmRezervations from './confirmRezervation.handlebars'
 
 export default (params) => {
     let rezervation = null;
@@ -14,12 +14,11 @@ export default (params) => {
         Header()
             .then(() => Footer())
             .then(() => Menkule.get('/rezervations/' + params.id))
-            .do((rez) => rezervation = rez).do(() => rezervation.updated_date = rezervation.updated_date == null ? false : true)
-            .then((rez) => $('body').zone('content').setContentAsync(Rezervation(rez)))
+            .then((rez) => rezervation = rez)
+            .then(() => rezervation.updated_date = rezervation.updated_date == null ? false : true)
+            .then(() => $('body').zone('content').setContentAsync(Rezervation(rezervation)))
             .then((template) => {
-                /*
-                    Message
-                 */
+                // Message
                 template.find('.message').on('click', function(e) {
                     e.preventDefault();
                     Message({
@@ -27,51 +26,49 @@ export default (params) => {
                         Id: rezervation.user_information.id
                     });
                 });
-                /*
-                Approved rezervation
-                 */
+                // Approved rezervation
                 template.find('.acceptbtn').on('click', function(e) {
                     let modal;
                     e.preventDefault();
                     Confirm({message: AppMessages ('rezervation_approved_confirm'), title: AppMessages('rezervation_approved_title')}).do(m => modal = m)
                         .then(() => Menkule.get('/rezervations/approve/' + rezervation.id))
                         .then(() => App.notifySuccess('Rezervasyon onaylandı!',''))
-                        .then(() => _Rezervation(params))
+                        .then(() => Rezervation(params))
                         .then(() => modal.modal('hide'))
                         .catch((err) => {
-                            App.notifyDanger(err.responseJSON.Message, 'Üzgünüz')
-                                .then(() => modal.modal('hide'))
+                            if(err.status === 501) {
+                                const rezervationConfirmMessage = ConfirmRezervations({rezervations: err.responseJSON});
+                                    App.promise(() => modal.modal('hide'))
+                                    .then(() => Confirm({title: 'Önemli Uyarı', message: rezervationConfirmMessage}))
+                                    .then(() => console.log('send'))
+                            } else {
+                                App.notifyDanger(err.responseJSON.Message, '')
+                                    .then(() => modal.modal('hide'))
+                            }
+
                         })
                 });
-
-                /*
-              Cancel rezervation
-               */
+                // Cancel rezervation
                 template.find('.cancelbtn').on('click', function(e) {
                     let modal;
                     e.preventDefault();
                     Confirm({message: AppMessages ('rezervation_cancel_confirm'), title: AppMessages('rezervation_cancel_title')}).do(m => modal = m)
                         .then(() => Menkule.get('/rezervations/cancel/' + rezervation.id))
                         .then(() => App.notifySuccess('Rezervasyon iptal edildi!',''))
-                        .then(() => _Rezervation(params))
+                        .then(() => Rezervation(params))
                         .then(() => modal.modal('hide'))
                         .catch((err) => {
                             App.notifyDanger(err.responseJSON.Message, 'Üzgünüz')
                                 .then(() => modal.modal('hide'))
                         })
                 });
-
-                /*
-                    Render Images
-                */
+                // Render Images
                 template.zone('images').setContentAsync(Images({images: rezervation.rezervation_advert.images, price: '250'}))
                     .then(() => App.promise(() => template.find("[data-fancybox]").fancybox( { buttons : ['close']})))
                     .then(() => {
                         template.find('.image-counter').on('click', e => $.fancybox.open($("[data-fancybox]"), { buttons : ['close']}))
                     });
-                /*
-                Create map and center
-                 */
+                // Create map and center
                 template.find("#map").createMap({scroll:true});
                 template.find("#map").centerTo({
                     'lat': rezervation.rezervation_advert.advert.latitude,
@@ -80,15 +77,15 @@ export default (params) => {
                     'lat': rezervation.rezervation_advert.advert.latitude,
                     'lng': rezervation.rezervation_advert.advert.longitude
                 });
-                    /*
-                  Disable add marker
-                   */
-                    template.find("#map").on('pin.map', function(e) {
-                        e.preventDefault();
-                    });
-
+                // Disable add marker
+                template.find("#map").on('pin.map', function(e) {
+                    e.preventDefault();
+                });
             })
             .then(() => resolve())
             .catch(err => console.log(err))
     })
 }
+
+
+
