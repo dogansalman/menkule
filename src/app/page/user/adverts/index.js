43ea0ef5c162bd9313baf0c1d70270c2c4612bdb
dogansalman <dogan.salman@yahoo.com>
2018-04-16 16:00:59 +0300

    tasarim iyilestirmeleri

--- src/app/page/user/adverts/index.js
+++ src/app/page/user/adverts/index.js
@@ -10,6 +10,16 @@ export default () => {
       .then(() => Footer())
       .then(() => Menkule.get("/adverts"))
       .then((adverts) =>  adverts.length == 0 ?  $("body").zone('content').setContentAsync(appMessages('advert_not_found')) : $("body").zone('content').setContentAsync(template({adverts})))
+        .then((template) => {
+
+          template.find('.list.advert').on('click',(e) => {
+            if(!$(e.target).closest('.link.detail').length)  App.navigate('/user/advert/' + $(e.target).closest('.advert').attr('data-item'), {}, true)
+          })
+            template.find('.link.detail').on('click', (e) => {
+                if($(e.target).closest('.link.detail').length) App.navigate('/advert/' + $(e.target).closest('.advert').attr('data-item'), {}, true)
+
+            })
+        })
       .catch(err => {
           $("body").zone('content').setContentAsync(appMessages('error_adverts'));
       });
